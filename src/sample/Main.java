548577fb69ed5bd186206a71414af8b27207c7d1
pllysun <1125887000@qq.com>
2024-06-23 23:22:15 +0800

    提交

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import sample.model.TouristSpot;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("游客行程生成工具");
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("旅途.png")));
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
